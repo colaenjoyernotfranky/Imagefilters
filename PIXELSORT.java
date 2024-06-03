636a65209e7f17e53a0e9db8c29e5f422d204d6c
chest-brah <bosnalp@hotmail.com>
2024-06-04 01:05:25 +0200

    Added masking

--- PIXELSORT.java
+++ PIXELSORT.java
@@ -12,26 +12,20 @@ public class PIXELSORT {
     public PIXELSORT() {
 
     }
-    public void sortPixels(String pathname) throws IOException {
+    public void sortPixels(String pathname, String maskPath) throws IOException {
         String outName = pathname.substring(pathname.lastIndexOf("/") + 1, pathname.lastIndexOf("."));
         BufferedImage img = getInputImage(pathname); // input image
-        ArrayList<ArrayList<Color>> pixelArray = new ArrayList<ArrayList<Color>>();
-        for (int x = 0; x < img.getWidth(); x++) { // "height pointer"
-            ArrayList<Color> tempArray = new ArrayList<Color>();
-            for (int y = 0; y < img.getHeight(); y++) { // "width pointer"
-                tempArray.add(new Color(img.getRGB(x, y)));
-            }
-            Collections.sort(tempArray, (o1, o2) -> singleValue(o1.getRGB()) - singleValue(o2.getRGB())); // BZZZZZTT
-            pixelArray.add(tempArray);
-        }
-        BufferedImage sortedImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
-        for (int x = 0; x < img.getWidth(); x++) { // "height pointer"
-            for (int y = 0; y < img.getHeight(); y++) { // "width pointer"
-                sortedImage.setRGB(x, y, pixelArray.get(x).get(y).getRGB());
-            }
-        }
-        ImageIO.write(sortedImage, "png", new File("./sorted_" + outName + ".png"));
+        BufferedImage maskImg = getInputImage(maskPath); // mask
+
+        ArrayList<Color> imgArray = extractImg(img);
+        ArrayList<Boolean> mask = extractMask(maskImg);
+        ArrayList<ArrayList<Color>> pixel = sort(img);
+
+        ImageIO.write(applyMask(img, pixel, mask, imgArray), "png", new File("./sorted_" + outName + ".png"));
+        System.out.println("Done");
     }
+
+    //Util-Functions//////////////////////////////////////////////////////////////////////////////////////////
     private BufferedImage getInputImage(String pathname) throws FileNotFoundException { // gets the image file
         BufferedImage img = null;
         // getting the image file
@@ -50,13 +44,66 @@ public class PIXELSORT {
         return ((r * 299 + g * 587 + b * 114) / 1000);
     }
 
+    private ArrayList<Boolean> extractMask(BufferedImage maskImg) {
+        ArrayList<Boolean> mask = new ArrayList<Boolean>();
+        for (int x = 0; x < maskImg.getWidth(); x++) { // "height pointer"
+            for (int y = 0; y < maskImg.getHeight(); y++) { // "width pointer"
+                boolean maskBoolean = false; 
+                if(maskImg.getRGB(x, y) == 0xFF000000)
+                    maskBoolean = false;
+                if(maskImg.getRGB(x, y) == 0xFFFFFFFF)
+                    maskBoolean = true;
+                mask.add(maskBoolean);
+            }
+        }
+        return mask;
+    }
+
+    private ArrayList<Color> extractImg(BufferedImage bufferedImage) {
+        ArrayList<Color> img = new ArrayList<Color>();
+        for (int x = 0; x < bufferedImage.getWidth(); x++) { // "height pointer"
+            for (int y = 0; y < bufferedImage.getHeight(); y++) { // "width pointer"
+                img.add(new Color(bufferedImage.getRGB(x, y)));
+            }
+        }
+        return img;
+    }
+
+    private ArrayList<ArrayList<Color>> sort(BufferedImage bufferedImage) {
+        ArrayList<ArrayList<Color>> sortedImgArray = new ArrayList<ArrayList<Color>>();
+        for (int x = 0; x < bufferedImage.getWidth(); x++) { // "height pointer"
+            ArrayList<Color> tempArray = new ArrayList<Color>();
+            for (int y = 0; y < bufferedImage.getHeight(); y++) { // "width pointer"
+                tempArray.add(new Color(bufferedImage.getRGB(x, y)));
+            }
+            Collections.sort(tempArray, (o1, o2) -> singleValue(o1.getRGB()) - singleValue(o2.getRGB()));
+            sortedImgArray.add(tempArray);
+        }
+        return sortedImgArray;
+    }
+
+    private BufferedImage applyMask(BufferedImage bufferedImage, ArrayList<ArrayList<Color>> sortedImgArray, ArrayList<Boolean> mask, ArrayList<Color> img) {
+        BufferedImage sortedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+        int index = 0;
+        for (int x = 0; x < bufferedImage.getWidth(); x++) { // "height pointer"
+            for (int y = 0; y < bufferedImage.getHeight(); y++) { // "width pointer"
+                if(mask.get(index))
+                    sortedImage.setRGB(x, y, img.get(index).getRGB());
+                if(!mask.get(index))
+                    sortedImage.setRGB(x, y, sortedImgArray.get(x).get(y).getRGB());
+                index++;
+            }
+        }
+        return sortedImage;
+    }
+
     public static void main(String args[]) {
         try {
             PIXELSORT s = new PIXELSORT();
-            s.sortPixels(args[0]);
+            s.sortPixels(args[0], args[1]);
         } catch (Exception e) {
-            System.out.println("Usage: java PIXELSORT <pathname> " + args[0]);
-            System.out.println("False arguments or missing pathname");
+            System.out.println("Usage: java PIXELSORT <image> <mask>");
+            System.out.println("False arguments or missing image or mask pathname");
         }
     }
 }
