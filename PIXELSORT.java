4c227f0916abb432e57e60a1436993ba68a7a003
colaenjoyernotfranky <bosnalp@hotmail.com>
2024-11-05 00:59:04 +0100

    Cleaned up PIXELSORT.java. Renamed a few things and added progress bar

--- PIXELSORT.java
+++ PIXELSORT.java
@@ -2,108 +2,193 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class PIXELSORT {
-    public PIXELSORT() {
+    private int bufferedImageWidth;
+    private int bufferedImageHeight;
+    private String progressString;
 
-    }
-    public void sortPixels(String pathname, String maskPath) throws IOException {
+    private PIXELSORT() {}
+
+    void sortPixels(String pathname, String maskPath) throws IOException {
         String outName = pathname.substring(pathname.lastIndexOf("/") + 1, pathname.lastIndexOf("."));
-        BufferedImage img = getInputImage(pathname); // input image
-        BufferedImage maskImg = getInputImage(maskPath); // mask
 
-        ArrayList<Color> imgArray = extractImg(img);
-        ArrayList<Boolean> mask = extractMask(maskImg);
-        ArrayList<ArrayList<Color>> pixel = sort(img);
+        BufferedImage imageToSort = getInputImage(pathname);
 
-        ImageIO.write(applyMask(img, pixel, mask, imgArray), "png", new File("./sorted_" + outName + ".png"));
-        System.out.println("Done");
+        bufferedImageWidth = imageToSort.getWidth();
+        bufferedImageHeight = imageToSort.getHeight();
+
+        Color[][] imgArray = extractImg(imageToSort);
+
+        updateProgressString(20);
+
+        BufferedImage imageMask;
+        boolean[][] mask = null;
+
+        if(maskPath != null) {
+            imageMask = getInputImage(maskPath);
+            mask = extractMask(imageMask);
+        }
+
+        updateProgressString(40);
+
+        Color[][] sortedPixels = sort(imageToSort);
+
+        updateProgressString(90);
+
+        ImageIO.write(applyMask(imgArray, sortedPixels, mask), "png", new File("./sorted_" + outName + ".png"));
+        updateProgressString(100);
     }
 
-    //Util-Functions//////////////////////////////////////////////////////////////////////////////////////////
-    private BufferedImage getInputImage(String pathname) throws FileNotFoundException { // gets the image file
+    private BufferedImage getInputImage(String pathname) throws FileNotFoundException {
         BufferedImage img = null;
-        // getting the image file
+
         try {
-            img = ImageIO.read(new File(pathname)); // set the file at pathname as image
+            img = ImageIO.read(new File(pathname));
         } catch (IOException e) {
             System.out.println(e);
         }
         return img;
     }
 
-    private int singleValue(int rgbValue) {
+    private int rgbAsSingleValue(int rgbValue) {
         int r = (rgbValue >> 16) & 0xff;
-        int g = (rgbValue >> 8) & 0xff; // getting the rgb values
+        int g = (rgbValue >> 8) & 0xff;
         int b = rgbValue & 0xff;
+
         return ((r * 299 + g * 587 + b * 114) / 1000);
     }
 
-    private ArrayList<Boolean> extractMask(BufferedImage maskImg) {
-        ArrayList<Boolean> mask = new ArrayList<Boolean>();
-        for (int x = 0; x < maskImg.getWidth(); x++) { // "height pointer"
-            for (int y = 0; y < maskImg.getHeight(); y++) { // "width pointer"
-                boolean maskBoolean = false; 
-                if(maskImg.getRGB(x, y) == 0xFF000000)
-                    maskBoolean = false;
-                if(maskImg.getRGB(x, y) == 0xFFFFFFFF)
-                    maskBoolean = true;
-                mask.add(maskBoolean);
+    private boolean[][] extractMask(BufferedImage maskImg) {
+        boolean[][] mask = new boolean[bufferedImageWidth][bufferedImageHeight];
+
+        for (int x = 0; x < bufferedImageWidth; x++) {
+            for (int y = 0; y < bufferedImageHeight; y++) {
+                if(maskImg.getRGB(x, y) == 0xFF000000) {
+                    mask[x][y] =  false;
+                }
+                if(maskImg.getRGB(x, y) == 0xFFFFFFFF) {
+                    mask[x][y] =  true;
+                }
             }
         }
         return mask;
     }
 
-    private ArrayList<Color> extractImg(BufferedImage bufferedImage) {
-        ArrayList<Color> img = new ArrayList<Color>();
-        for (int x = 0; x < bufferedImage.getWidth(); x++) { // "height pointer"
-            for (int y = 0; y < bufferedImage.getHeight(); y++) { // "width pointer"
-                img.add(new Color(bufferedImage.getRGB(x, y)));
+    private Color[][] extractImg(BufferedImage bufferedImage) {
+        Color[][] img = new Color[bufferedImageWidth][bufferedImageHeight];
+
+        for (int x = 0; x < bufferedImageWidth; x++) {
+            for (int y = 0; y < bufferedImageHeight; y++) {
+                img[x][y] = new Color(bufferedImage.getRGB(x, y));
             }
         }
         return img;
     }
 
-    private ArrayList<ArrayList<Color>> sort(BufferedImage bufferedImage) {
-        ArrayList<ArrayList<Color>> sortedImgArray = new ArrayList<ArrayList<Color>>();
-        for (int x = 0; x < bufferedImage.getWidth(); x++) { // "height pointer"
-            ArrayList<Color> tempArray = new ArrayList<Color>();
-            for (int y = 0; y < bufferedImage.getHeight(); y++) { // "width pointer"
-                tempArray.add(new Color(bufferedImage.getRGB(x, y)));
+    private Color[][] sort(BufferedImage bufferedImage) {
+        Color[][] sortedImgArray = new Color[bufferedImageWidth][bufferedImageHeight];
+
+        for (int x = 0; x < bufferedImageWidth; x++) {
+            Color[] tempArray = new Color[bufferedImageHeight];
+
+            for (int y = 0; y < bufferedImageHeight; y++) {
+                tempArray[y] = new Color(bufferedImage.getRGB(x, y));
             }
-            Collections.sort(tempArray, (o1, o2) -> singleValue(o1.getRGB()) - singleValue(o2.getRGB()));
-            sortedImgArray.add(tempArray);
+
+            ArrayList<Color> tempArrayAsList = new ArrayList<>(Arrays.asList(tempArray));
+
+            Collections.sort(tempArrayAsList, (o1, o2) -> rgbAsSingleValue(o1.getRGB()) - rgbAsSingleValue(o2.getRGB()));
+
+            for (int y = 0; y < bufferedImageHeight; y++) {
+                tempArray[y] = tempArrayAsList.get(y);
+            }
+            
+            sortedImgArray[x] = tempArray;
         }
         return sortedImgArray;
     }
 
-    private BufferedImage applyMask(BufferedImage bufferedImage, ArrayList<ArrayList<Color>> sortedImgArray, ArrayList<Boolean> mask, ArrayList<Color> img) {
-        BufferedImage sortedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-        int index = 0;
-        for (int x = 0; x < bufferedImage.getWidth(); x++) { // "height pointer"
-            for (int y = 0; y < bufferedImage.getHeight(); y++) { // "width pointer"
-                if(mask.get(index))
-                    sortedImage.setRGB(x, y, img.get(index).getRGB());
-                if(!mask.get(index))
-                    sortedImage.setRGB(x, y, sortedImgArray.get(x).get(y).getRGB());
-                index++;
+    private BufferedImage applyMask(Color[][] img, Color[][] sortedImgArray, boolean[][] mask) {
+        BufferedImage sortedImage = new BufferedImage(bufferedImageWidth, bufferedImageHeight, BufferedImage.TYPE_INT_RGB);
+
+        if(mask == null) {
+            for (int x = 0; x < bufferedImageWidth; x++) {
+                for (int y = 0; y < bufferedImageHeight; y++) {
+                    sortedImage.setRGB(x, y, sortedImgArray[x][y].getRGB());
+                }
             }
+            return sortedImage;
+        } else {
+            for (int x = 0; x < bufferedImageWidth; x++) {
+                for (int y = 0; y < bufferedImageHeight; y++) {
+                    if(mask[x][y]) {
+                        sortedImage.setRGB(x, y, img[x][y].getRGB());
+                    }
+                    if(!mask[x][y]) {
+                        sortedImage.setRGB(x, y, sortedImgArray[x][y].getRGB());
+                    }
+                }
+            }
+            return sortedImage;
         }
-        return sortedImage;
+    }
+
+    private void updateProgressString(int percentage) {
+        String progressBar = "";
+
+        for(int i = 0; i < (percentage / 5); i++) {
+            progressBar += "=";
+        }
+
+        int emptySpaceLength = (20 - progressBar.length());
+
+        for(int i = 0; i < emptySpaceLength; i++) {
+            progressBar += " ";
+        }
+
+        if(progressString != null) {
+            String backspaceString = "";
+            for(int i = 0; i < progressString.length(); i++) {
+                backspaceString += "\b";
+            }
+            System.out.print(backspaceString);
+        }
+
+        progressString = "[" + progressBar + "] " + percentage + "%";
+
+        System.out.print(progressString);
+        if(percentage == 100) {
+            System.out.println();
+            System.out.println("DONE.");
+        }
     }
 
     public static void main(String args[]) {
+        PIXELSORT pixelsort = new PIXELSORT();
+
+        String imagePath = null;
+        String maskPath = null;
+
+        if(args.length > 1) {
+            imagePath = args[0];
+            maskPath = args[1];
+        } else if (args.length == 1) {
+            imagePath = args[0];
+        } else {
+            System.out.println("Usage: java PIXELSORT <image> [mask]");
+        }
+
         try {
-            PIXELSORT s = new PIXELSORT();
-            s.sortPixels(args[0], args[1]);
+            pixelsort.sortPixels(imagePath, maskPath);
         } catch (Exception e) {
-            System.out.println("Usage: java PIXELSORT <image> <mask>");
-            System.out.println("False arguments or missing image or mask pathname");
+            System.out.println(e.getMessage());
         }
     }
 }
