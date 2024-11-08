e5b45865ed1ab662daf35eebee1357f3af097e9d
colaenjoyernotfranky <bosnalp@hotmail.com>
2024-11-08 17:22:22 +0100

    Implemented environment variables usage

--- src/main/java/com/colaenjoyer/imagefilters/configuration/FilterConfiguration.java
+++ src/main/java/com/colaenjoyer/imagefilters/configuration/FilterConfiguration.java
@@ -1,13 +1,11 @@
 package com.colaenjoyer.imagefilters.configuration;
 
-public class FilterConfiguration {
+public final class FilterConfiguration {
     private FilterConfiguration() {}
 
-    public static class AsciiFilterConfiguration {
-        //TODO: Get from config file
-        private static final int FONT_SIZE = 8;
-        private static final int BRIGHTNESS_OFFSET = 0;
-        private static final int MAX_PIXEL_SHIFT = 50;
+    public static final class AsciiFilterConfiguration {
+        private static final int FONT_SIZE = getEnvironmentVariable("FONT_SIZE", 8);
+        private static final int BRIGHTNESS_OFFSET = getEnvironmentVariable("BRIGHTNESS_OFFSET", 0);
 
         private AsciiFilterConfiguration() {}
 
@@ -20,12 +18,28 @@ public class FilterConfiguration {
         }
     }
 
-    public static class PixelsortConfiguration {
+    public static final class PixelsortConfiguration {
+        private static final int MAX_PIXEL_SHIFT = getEnvironmentVariable("MAX_PIXEL_SHIFT", 50);
 
         private PixelsortConfiguration() {}
 
         public static int getMaxPixelShift() {
-            return AsciiFilterConfiguration.MAX_PIXEL_SHIFT;
+            return MAX_PIXEL_SHIFT;
         }
+    }
+
+    private static int getEnvironmentVariable(String variableName, int defaultValue) {
+        return (System.getProperty(variableName) == null || System.getProperty(variableName).isEmpty()) ?
+                defaultValue : Integer.parseInt(System.getProperty(variableName));
+    }
+
+    private static String getEnvironmentVariable(String variableName, String defaultValue) {
+        return (System.getProperty(variableName) == null || System.getProperty(variableName).isEmpty()) ?
+                defaultValue : System.getProperty(variableName);
+    }
+
+    private static double getEnvironmentVariable(String variableName, double defaultValue) {
+        return (System.getProperty(variableName) == null || System.getProperty(variableName).isEmpty()) ?
+                defaultValue : Double.parseDouble(System.getProperty(variableName));
     }
 }
